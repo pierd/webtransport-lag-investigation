dacab1f95bbf60e73f2f4a00b197cdabb8c9000a
Kuba Jaroszewski <jakub.jaroszewski@gmail.com>
2023-11-14 21:18:39 +0100

    Add webtransport-in-worker-transferred-stream

---
+++ web/webtransport-in-worker-transferred-stream/index.js
@@ -0,0 +1,43 @@
+import * as common from "/common.js";
+
+const worker = new Worker("worker.js", { type: "module" });
+
+const animationFrameCallback = () => {
+    common.onFrame();
+    common.busyWait(50);
+
+    for (let i = 0; i < 10; i++) {
+        worker.postMessage(common.nowAsBuffer());
+    }
+
+    requestAnimationFrame(animationFrameCallback);
+};
+
+function handleData(data, report) {
+    if (data instanceof Uint8Array) {
+        const now = common.shortNow();
+        const timestamp = common.timestampFromBuffer(data);
+        report(now - timestamp);
+    } else {
+        report(data);
+    }
+}
+
+export async function run() {
+    common.initUI();
+    animationFrameCallback();
+
+    worker.onmessage = (e) => {
+        if (e.data.data !== undefined) {
+            handleData(e.data.data, common.reportLatency);
+        } else if (e.data.stream !== undefined) {
+            common.createReaderCallback(e.data.stream.getReader(), common.reportStream)();
+        } else if (e.data.write !== undefined) {
+            common.reportWriteTime(e.data.write);
+        } else if (e.data.error !== undefined) {
+            common.reportError(e.data.error);
+        } else {
+            console.log(e.data);
+        }
+    };
+}
---
+++ web/webtransport-in-worker-transferred-stream/worker.js
@@ -0,0 +1,54 @@
+import * as common from "/common.js";
+
+var writer = null;
+
+function reportWriteTime(write) {
+    postMessage({ write });
+}
+
+function reportDatagram(data) { 
+    postMessage({ data });
+}
+
+function reportError(error) {
+    postMessage({ error });
+}
+
+function createReaderCallback(reader, reportCallback) {
+    const report = reportCallback || reportDatagram;
+    const callback = () => {
+        reader.read().then(({ value, done }) => {
+            report(value);
+            callback();
+        }, (e) => {
+            report(e.message);
+        });
+    }
+    return callback;
+}
+
+onmessage = function (e) {
+    if (writer !== null) {
+        const now = common.shortNow();
+        writer.write(e.data).then(() => {
+            reportWriteTime(common.shortNow() - now);
+        }, (e) => {
+            reportWriteTime(e.message);
+        });
+    } else {
+        console.error("Got data but no stream is available");
+    }
+};
+
+let w = new WebTransport("https://127.0.0.1:9000");
+w.ready.then(() => {
+    writer = w.datagrams.writable.getWriter();
+    createReaderCallback(w.datagrams.readable.getReader())();
+    w.incomingUnidirectionalStreams.getReader().read().then(({ value, done }) => {
+        postMessage({ stream: value }, [value]);
+    });
+}, (e) => {
+    reportError(e.message);
+});
+
+console.info("Worker started");
